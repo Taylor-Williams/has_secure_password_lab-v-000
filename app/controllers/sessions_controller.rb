ed9a92d048357f377362f6e8789f25612ea59a93
Taylor Williams <taylor@thewilliams.net>
2019-01-02 23:47:46 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
-    return head(:forbidden) unless @user && @user.authenticate(params[:password])
+    return head(:forbidden) unless @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
     redirect_to user_path(@user)
   end
