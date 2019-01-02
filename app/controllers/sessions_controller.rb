938c54eaabda8991d8e68a1a1107047bd38fc0b5
Taylor Williams <taylor@thewilliams.net>
2019-01-02 23:45:34 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,6 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:name])
     return head(:forbidden) unless @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 end
