eb1224f8626ca609aa80d0482dad8f7d02258653
Taylor Williams <taylor@thewilliams.net>
2019-01-02 23:05:06 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ActiveRecord::Base
-  validate :password_validator
 
-  def password_validator
-    self.password_confirmation == self.password if self.password_confirmation
-  end
 end
