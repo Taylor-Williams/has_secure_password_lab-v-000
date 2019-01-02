4e62bd8f7a4ad03fcdd85770c1c4f48b350f0c6f
Taylor Williams <taylor@thewilliams.net>
2019-01-02 23:19:14 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(name: params[:name])
+    @user = User.create(user_params)
   end
 
   def show
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
